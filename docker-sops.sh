c4bca52d276e0df3edcc2277ec0bb3c269c32c01
ben <koppe.development@gmail.com>
2025-12-18 17:29:55 -0500

    Utilize TMP_KEY to workaround docker bind mount limitations

--- docker-sops.sh
+++ docker-sops.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
+TMP_KEY=$(mktemp)
+cp /config/komodo-syncs "$TMP_KEY"
+chmod 600 "$TMP_KEY"
+
 docker run --rm \
   -v "$(pwd):/work" \
-  -v "/config/komodo-syncs:/root/.ssh/id_ed25519" \
+  -v "$TMP_KEY:/root/.ssh/id_ed25519:ro" \
   -w /work \
   rayou/sops:latest \
   "$@"
+
+rm -f "$TMP_KEY"
