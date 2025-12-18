b59c7ad76d03696301f24a13f67cae1a457b4e6e
ben <koppe.development@gmail.com>
2025-12-18 16:58:58 -0500

    Accept inputs to docker-sops.sh

--- docker-sops.sh
+++ docker-sops.sh
@@ -1,1 +1,8 @@
-docker run --rm -v "$(pwd):/work" -v "/config/komodo-syncs:/root/.ssh/id_ed25519" -w /work rayou/sops:latest
+#!/usr/bin/env bash
+
+docker run --rm \
+  -v "$(pwd):/work" \
+  -v "/config/komodo-syncs:/root/.ssh/id_ed25519" \
+  -w /work \
+  rayou/sops:latest \
+  "$@"
