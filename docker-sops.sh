03b5409ff991c9b7a6b8d9c683e842504aab5dfa
ben <koppe.development@gmail.com>
2025-12-19 10:50:21 -0500

    Simplify docker-sops.sh to use env vars instead of files

--- docker-sops.sh
+++ docker-sops.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 
-TMP_KEY=$(mktemp)
-cp /config/komodo-syncs "$TMP_KEY"
-chmod 600 "$TMP_KEY"
-
 docker run --rm \
   -v "$(pwd):/work" \
   -v "$TMP_KEY:/root/.ssh/id_ed25519:ro" \
+  -e SOPS_AGE_KEY="$(cat /config/komodo-syncs)" \
   -w /work \
   rayou/sops:latest \
   "$@"
-
-rm -f "$TMP_KEY"
